1b1a2cd03dd4197d78f671cdcec8eefdb2334efd
Justin Barrick <justin.m.barrick@gmail.com>
2019-02-19 19:52:16 -0800

    Improve kubeconfig loading to use built-in client-go methods.

--- pkg/kube/clientset.go
+++ pkg/kube/clientset.go
@@ -15,10 +15,6 @@
 package kube
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
-
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -49,28 +45,8 @@ func NewMetricsClientSet() (*metrics.Clientset, error) {
 }
 
 func getKubeConfig() (*rest.Config, error) {
-	var kubeconfig string
-	if os.Getenv("KUBECONFIG") != "" {
-		kubeconfig = os.Getenv("KUBECONFIG")
-	} else if home := homeDir(); home != "" {
-		kubeconfig = filepath.Join(home, ".kube", "config")
-	} else {
-		fmt.Println("Parsing kubeconfig failed, please set KUBECONFIG env var")
-		os.Exit(1)
-	}
-
-	if _, err := os.Stat(kubeconfig); err != nil {
-		// kubeconfig doesn't exist
-		fmt.Printf("%s does not exist - please make sure you have a kubeconfig configured.\n", kubeconfig)
-		panic(err.Error())
-	}
-
-	return clientcmd.BuildConfigFromFlags("", kubeconfig)
-}
-
-func homeDir() string {
-	if h := os.Getenv("HOME"); h != "" {
-		return h
-	}
-	return os.Getenv("USERPROFILE") // windows
+	return clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		clientcmd.NewDefaultClientConfigLoadingRules(),
+		&clientcmd.ConfigOverrides{},
+	).ClientConfig()
 }
