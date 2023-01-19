fe1c2748e12fe67e286a23cdb8605ff7cb84fcc5
Rob Scott <rob@robertjscott.ca>
2023-01-18 22:58:48 -0800

    Bumping version to v0.7.2, upgrading go modules, moving to GitHub actions

--- pkg/capacity/capacity_test.go
+++ pkg/capacity/capacity_test.go
@@ -18,10 +18,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	"k8s.io/client-go/kubernetes/fake"
 )
 
